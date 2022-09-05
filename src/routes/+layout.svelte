53d20518c1364fee5508bfd6012085198b9a21a1
David M. Fajardo <davidmf99@gmail.com>
2022-09-05 17:41:19 +0200

    footer and header inside layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,53 @@
 <script lang="ts">
     import '../app.css'
-    import Header from '$lib/Header.svelte'
-    import Footer from '$lib/Footer.svelte'
+    import logo from '/favicon.png'
+    const date = new Date().getFullYear()
 </script>
 
 <div class="lg:h-screen bg-zinc-900 text-white">
-    <header class="bg-zinc-800">
-        <Header />
+    <header class="">
+        <nav class="bg-zinc-800 p-4 flex justify-between items-center border border-x-0 border-t-0 ">
+            <div class="flex items-center">
+                <img class="w-12" src={logo} alt="Svelte Logo" />
+                <h1 class="text-xl ml-5">Svelte App</h1>
+            </div>
+
+            <div class="text-2xl hover:text-yellow-300">
+                <a title="See project" href="https://github.com/dawichi/duckgames" target="_blank" rel="noreferrer">
+                    <i class="bi bi-github" />
+                </a>
+            </div>
+        </nav>
     </header>
-    
+
     <main class="mt-10 px-2">
         <slot />
     </main>
-    
-    
-    <div class="bg-zinc-800 lg:absolute bottom-0 left-0 right-0 text-center">
-        <Footer />
-    </div>
+
+    <footer class="bg-zinc-800 lg:absolute bottom-0 left-0 right-0 text-center">
+        <div class="container m-auto flex flex-col md:flex-row justify-around items-center">
+            <a href="/">
+                <section class="flex flex-col hover:text-yellow-300">
+                    <span class="text-xl">다 위 치</span>
+                    <span>DuckGames © {date}</span>
+                </section>
+            </a>
+
+            <a target="_blank" href="https://www.buymeacoffee.com/dawichi" rel="noreferrer">
+                <img
+                    class="w-64 h-24"
+                    src="https://img.buymeacoffee.com/button-api/?text=Buy%20me%20a%20coffee&emoji=&slug=dawichi&button_colour=fde047&font_colour=000&font_family=Cookie&outline_colour=000000&coffee_colour=fff"
+                    alt="Coffee"
+                />
+            </a>
+        </div>
+    </footer>
 </div>
+
+<style>
+    nav {
+        position: sticky;
+        top: 0;
+        z-index: 1;
+    }
+</style>
--- src/routes/tictactoe/+page.svelte
+++ src/routes/tictactoe/+page.svelte
@@ -1,1 +1,50 @@
-<h1>hi</h1>
+<script lang="ts">
+    const game: {
+        data: {
+            board: number[]
+        }
+        player1: number[]
+        player2: number[]
+    } = {
+        data: {
+            board: [],
+        },
+        player1: [],
+        player2: [],
+    }
+
+    
+    game.data.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    game.player1 = [1, 1, 1, 2, 2, 2, 3, 3, 3]
+    game.player2 = [1, 1, 1, 2, 2, 2, 3, 3, 3]
+</script>
+
+<div>
+    <h1 class="text-3xl text-center py-5 font-bold">Tic Tac Toe</h1>
+    <hr />
+
+    <div class="w-full grid grid-rows-3 gap-4 mt-12">
+        <div class="grid grid-cols-3 gap-4">
+            <div class="border h-24 w-24 flex items-center justify-center">{game.data.board[0]}</div>
+            <div class="border h-24 w-24 flex items-center justify-center">{game.data.board[1]}</div>
+            <div class="border h-24 w-24 flex items-center justify-center">{game.data.board[2]}</div>
+        </div>
+        <div class="grid grid-cols-3 gap-4">
+            <div class="border h-24 w-24 flex items-center justify-center">{game.data.board[3]}</div>
+            <div class="border h-24 w-24 flex items-center justify-center">{game.data.board[4]}</div>
+            <div class="border h-24 w-24 flex items-center justify-center">{game.data.board[5]}</div>
+        </div>
+        <div class="grid grid-cols-3 gap-4">
+            <div class="border h-24 w-24 flex items-center justify-center">{game.data.board[6]}</div>
+            <div class="border h-24 w-24 flex items-center justify-center">{game.data.board[7]}</div>
+            <div class="border h-24 w-24 flex items-center justify-center">{game.data.board[8]}</div>
+        </div>
+    </div>
+
+    <div class="mt-5 grid grid-cols-2">
+        <h4>Player 1</h4>
+        <h4>Player 2</h4>
+        <p>{game.player1}</p>
+        <p>{game.player2}</p>
+    </div>
+</div>
