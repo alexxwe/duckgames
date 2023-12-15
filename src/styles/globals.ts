a2eaa366e5543e02458cdb1cfdf0865701424f67
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-12-15 12:50:30 +0100

    common styles infer types

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,10 +1,10 @@
 // Here are declared the common styles for all buttons
-const common: Record<string, string> = {
+const common = {
     button: 'text-xl tracking-wide py-1 px-6 m-2 rounded',
 }
 
 // Here are declared the specific styles for each type of button
-const specific: Record<string, Record<string, string>> = {
+const specific = {
     button: {
         info: 'bg-blue-500 hover:bg-blue-700 border-2 border-blue-800',
         success: 'bg-green-500 hover:bg-green-700 border-2 border-green-800',
@@ -18,10 +18,13 @@ const specific: Record<string, Record<string, string>> = {
 export const styles = new Proxy(specific, {
     get: (target, prop: string) => {
         if (prop in target) {
-            for (const key in target[prop]) {
-                target[prop][key] = `${common[prop]} ${target[prop][key]}`
+            const element = target[prop as keyof typeof target]
+
+            for (const key in element) {
+                element[key as keyof typeof element] = `${common[prop as keyof typeof target]} ${element[key as keyof typeof element]}`
             }
-            return target[prop]
+
+            return element
         }
     },
 })
